110caccf15a4f7c542fef9d9b223b38e7676f0d8
Cauchy <mailcauchy@gmail.com>
2022-10-08 16:47:44 +0800

    header and content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,27 @@
     <el-container>
       <el-header>
         <el-card class="box-card">
-
+          <el-row>
+            <el-col id="search_input_col">
+              <el-input placeholder="Search"/>
+            </el-col>
+            <el-col id="search_button_col">
+              <el-button>搜索</el-button>
+            </el-col>
+          </el-row>
         </el-card>
       </el-header>
       <el-main>
         <el-card class="box-card">
-
+          <div>
+            <el-row>第一行内容</el-row>
+            <el-row>第二行内容</el-row>
+            <el-row>第三行内容</el-row>
+          </div>
         </el-card>
       </el-main>
       <el-footer>
-        <el-card></el-card>
+        <el-card class="box-card"></el-card>
       </el-footer>
     </el-container>
   </div>
@@ -24,9 +35,18 @@ export default {
 }
 </script>
 <style scoped>
+#search_input_col {
+  max-width: 50%;
+}
+
+#search_button_col {
+  max-width: 50%;
+}
+
 .box-card {
   width: 60%;
-
-
+  margin: auto;
 }
+
+
 </style>
